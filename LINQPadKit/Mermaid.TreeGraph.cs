b640ea54d46a2fb1017e4549d29d1a8c637a5a20
zmjack <jack@zmland.com>
2023-10-07 16:31:17 +0800

    LINQPadKit.0.1.17

--- LINQPadKit/Mermaid.TreeGraph.cs
+++ LINQPadKit/Mermaid.TreeGraph.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections;
+using System.Text;
 
 namespace LINQPadKit;
 
@@ -6,10 +7,46 @@ public partial class Mermaid
 {
     public class TreeGraph : Mermaid
     {
+        public class TreeNode : ITreeNode, IEnumerable<ITreeNode>
+        {
+            public string Text { get; }
+            private readonly List<ITreeNode> _children = new();
+            public IEnumerable<ITreeNode> Children => _children;
+
+            public TreeNode()
+            {
+            }
+
+            public TreeNode(string text)
+            {
+                Text = text;
+            }
+
+            public void Add(ITreeNode node)
+            {
+                _children.Add(node);
+            }
+
+            public void Clear()
+            {
+                _children.Clear();
+            }
+
+            public IEnumerator<ITreeNode> GetEnumerator()
+            {
+                return _children.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return _children.GetEnumerator();
+            }
+        }
+
         public interface ITreeNode
         {
             string Text { get; }
-            ITreeNode[] Children { get; }
+            IEnumerable<ITreeNode> Children { get; }
         }
 
         private readonly Direction _direction;
@@ -32,18 +69,21 @@ public partial class Mermaid
         {
             void Build(StringBuilder sb, ITreeNode node)
             {
-                foreach (var child in node.Children)
+                if (node.Children is not null)
                 {
-                    if (child is not null)
+                    foreach (var child in node.Children)
                     {
-                        sb.AppendLine($"{node.GetHashCode()}(({node.Text})) --- {child.GetHashCode()}(({child.Text}))");
-                        Build(sb, child);
-                    }
-                    else
-                    {
-                        var empty = Guid.NewGuid();
-                        sb.AppendLine($"{node.GetHashCode()}(({node.Text})) ~~~ {empty}(( ))");
-                        sb.AppendLine($"style {empty} fill:#f100,stroke-width:0px");
+                        if (child is not null)
+                        {
+                            sb.AppendLine($"{node.GetHashCode()}(({node.Text})) --- {child.GetHashCode()}(({child.Text}))");
+                            Build(sb, child);
+                        }
+                        else
+                        {
+                            var empty = Guid.NewGuid();
+                            sb.AppendLine($"{node.GetHashCode()}(({node.Text})) ~~~ {empty}(( ))");
+                            sb.AppendLine($"style {empty} fill:transparent,stroke-width:0px");
+                        }
                     }
                 }
             }
--- README.md
+++ README.md
@@ -1,2 +1,90 @@
 # LINQPadKit
 Extension Toolkit for LINQPad.
+
+<br/>
+
+## Prism
+
+```csharp
+void Main()
+{
+	Prism.Import();    
+	new Prism("csharp")
+	{
+"""
+Console.WriteLine("Render code in LINQPad !");
+"""
+	}.Dump();
+}
+```
+
+```csharp
+Console.WriteLine("Render code in LINQPad !");
+```
+
+<br/>
+
+## Mermaid
+
+```csharp
+void Main()
+{
+	Mermaid.Import();    
+	new Mermaid
+	{
+"""
+graph TB
+A --> B
+A --> C
+"""
+	}.Dump("Mermaid");
+}
+```
+
+```mermaid
+graph TB
+A --> B
+A --> C
+```
+
+### TreeGraph
+
+```csharp
+void Main()
+{
+	Mermaid.Import();
+	new Mermaid.TreeGraph
+	{
+		new TreeNode("A")
+		{
+			new TreeNode("B")
+			{
+				new TreeNode("D"),
+				null,
+			},
+			new TreeNode("C")
+			{
+				null,
+				new TreeNode("E"),
+			},
+		}
+	}.Dump("Mermaid.TreeGraph");
+}
+```
+
+```mermaid
+graph TB
+A((A)) --- B((B))
+B((B)) --- D((D))
+B((B)) --- 8db619b9-315e-44aa-b101-75aeba2ceda2(( ))
+style 8db619b9-315e-44aa-b101-75aeba2ceda2 fill:transparent,stroke-width:0px
+A((A)) --- C((C))
+C((C)) --- 063a88a0-0be4-4d66-a5cb-7f3924fa682a(( ))
+style 063a88a0-0be4-4d66-a5cb-7f3924fa682a fill:transparent,stroke-width:0px
+C((C)) --- E((E))
+linkStyle 2 stroke-width:0px
+linkStyle 4 stroke-width:0px
+```
+
+
+
